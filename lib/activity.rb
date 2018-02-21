9df027b68c05b17a176a5b860dfd95355b4d6d44
pollockcl <pollock.clee@gmail.com>
2018-02-21 09:18:35 -0700

    finished iteration one

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,16 @@
+# Activity class
 class Activity
+  attr_reader :participants
+  def initialize(name, participants = {})
+    @name         = name
+    @participants = participants
+  end
+
+  def add_participants(name, amt_paid)
+    @participants[name] = amt_paid
+  end
+
+  def total_cost
+    @participants.values.sum
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,27 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_existence
+    participants = { 'Legolas' => 2 }
+    act = Activity.new('Taking the hobbits to Isengargd', participants)
+
+    assert_instance_of Activity, act
+  end
+
+  def test_add_participants
+    participants = { 'Legolas' => 2 }
+    act = Activity.new('Taking the hobbits to Isengargd', participants)
+
+    act.add_participants('Gimli', 3)
+
+    assert_equal 2, act.participants.size
+  end
+
+  def test_total_cost
+    participants = { 'Legolas' => 2,
+                     'Gimli'   => 1 }
+    act = Activity.new('Taking the hobbits to Isengargd', participants)
+
+    assert_equal 3, act.total_cost
+  end
 end
