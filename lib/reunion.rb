0af677fa8dbf9af2a619f99c2ff455ef863f4a13
pollockcl <pollock.clee@gmail.com>
2018-02-21 09:43:38 -0700

    iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,11 @@
 class Reunion
+  attr_reader :activities
+  def initialize(location)
+    @location   = location
+    @activities = []
+  end
+
+  def add_activity(activity)
+    @activities << activity
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -2,6 +2,24 @@ require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
+  def setup
+    @reunion = Reunion.new('Middle Earth')
+  end
+
+  def test_existence
+    assert_instance_of Reunion, @reunion
+  end
+
+  def test_add_activities
+    participants = { 'Legolas' => 2,
+                     'Gimli'   => 4 }
+    act = Activity.new('Taking the hobbits to Isengargd', participants)
+    @reunion.add_activity(act)
+
+    assert_instance_of Activity, @reunion.activities.first
+    assert_equal 1, @reunion.activities.size
+  end
 end
